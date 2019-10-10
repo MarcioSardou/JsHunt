814bb116c99990b04c4bcb1fae9ff3a7bda8edaa
Marcio Sardou <marciosut1996@gmail.com>
2019-10-10 18:20:57 -0300

    inicializing the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+
+
+const App = () => (
+  
+    <div className="App">
+        <Header />
+        <Footer />
+    </div>
+)
+
+export default App;
